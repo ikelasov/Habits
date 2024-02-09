b5fe9d6b7560b15bede5e35758ee7e0964a3503e
ilias.kelasidis <ikelasid@gmail.com>
2024-02-09 18:13:13 +0100

    Format code

--- app/src/main/java/com/example/habits/HabitsDestinations.kt
+++ app/src/main/java/com/example/habits/HabitsDestinations.kt
@@ -2,5 +2,6 @@ package com.example.habits
 
 sealed class HabitsDestinations(val route: String) {
     data object HabitsScreen : HabitsDestinations(route = "habits")
+
     data object AddHabitScreen : HabitsDestinations(route = "add_habit")
 }
