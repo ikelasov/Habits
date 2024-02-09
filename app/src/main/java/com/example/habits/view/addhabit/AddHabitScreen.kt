7175d92415faa5e99e25dcd18e456bc4834fabff
ilias.kelasidis <ikelasid@gmail.com>
2024-02-09 18:11:05 +0100

    Create empty AddHabitScreen

---
+++ app/src/main/java/com/example/habits/view/addhabit/AddHabitScreen.kt
@@ -0,0 +1,13 @@
+package com.example.habits.view.addhabit
+
+import androidx.compose.foundation.layout.Box
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+
+@Composable
+fun AddHabitScreen() {
+    Box(contentAlignment = Alignment.Center) {
+        Text("Hello nav")
+    }
+}
