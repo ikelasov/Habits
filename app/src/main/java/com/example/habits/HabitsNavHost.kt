35fd1ec80d0dada993fef17ecf803c587d155c87
ilias.kelasidis <ikelasid@gmail.com>
2024-02-09 18:12:03 +0100

    Create NavHost

---
+++ app/src/main/java/com/example/habits/HabitsNavHost.kt
@@ -0,0 +1,32 @@
+package com.example.habits
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import com.example.habits.view.addhabit.AddHabitScreen
+import com.example.habits.view.habits.HabitsScreen
+
+@Composable
+fun HabitsNavHost(
+    navController: NavHostController,
+    modifier: Modifier = Modifier,
+) {
+    NavHost(
+        navController = navController,
+        startDestination = HabitsDestinations.HabitsScreen.route,
+        modifier = modifier,
+    ) {
+        composable(route = HabitsDestinations.HabitsScreen.route) {
+            HabitsScreen(
+                onAddHabitClicked = {
+                    navController.navigate(HabitsDestinations.AddHabitScreen.route)
+                },
+            )
+        }
+        composable(route = HabitsDestinations.AddHabitScreen.route) {
+            AddHabitScreen()
+        }
+    }
+}
