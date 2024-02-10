9ea645b2a2b7826b5a334ed63649f4a7716fe49a
ilias.kelasidis <ikelasid@gmail.com>
2024-02-10 14:19:56 +0100

    Create BorderlessRoundedTextField

---
+++ app/src/main/java/com/example/habits/view/addhabit/screencomponents/BorderlessRoundedTextField.kt
@@ -0,0 +1,49 @@
+package com.example.habits.view.addhabit.screencomponents
+
+import androidx.compose.foundation.background
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Text
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.habits.R
+
+@ExperimentalMaterial3Api
+@Composable
+fun BorderlessRoundedTextField(
+    habitName: String,
+    hint: String,
+    onValueChanged: (String) -> Unit,
+    modifier: Modifier = Modifier,
+) {
+    OutlinedTextField(
+        value = habitName,
+        onValueChange = {
+            onValueChanged(it)
+        },
+        placeholder = { Text(text = hint) },
+        colors =
+        TextFieldDefaults.outlinedTextFieldColors(
+            focusedBorderColor = Color.Transparent,
+            unfocusedBorderColor = Color.Transparent,
+            placeholderColor = colorResource(R.color.slate_blue_gray),
+        ),
+        modifier =
+        modifier
+            .background(
+                color = Color.White,
+                shape = MaterialTheme.shapes.large,
+            ),
+    )
+}
+
+@Preview
+@Composable
+fun BorderlessRoundedTextFieldPreview() {
+    BorderlessRoundedTextFieldPreview()
+}
