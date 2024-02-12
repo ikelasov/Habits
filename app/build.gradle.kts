d9150dce303fe008a26e1437b26ea0b37179958a
ilias.kelasidis <ikelasid@gmail.com>
2024-02-12 16:12:19 +0100

    Update compose bom version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -71,7 +71,7 @@ dependencies {
 
     val lifecycleVersion = "2.7.0"
     implementation("androidx.activity:activity-compose:1.8.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    implementation(platform("androidx.compose:compose-bom:2024.02.00"))
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
