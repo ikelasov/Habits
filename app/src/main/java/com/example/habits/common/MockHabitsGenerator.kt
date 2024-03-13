ebb26f068035958ac30bfc29ec2f5c3b32c62cba
ilias.kelasidis <ikelasid@gmail.com>
2024-03-13 12:17:20 +0100

    Add reminder times in mock habit creation

--- app/src/main/java/com/example/habits/common/MockHabitsGenerator.kt
+++ app/src/main/java/com/example/habits/common/MockHabitsGenerator.kt
@@ -4,6 +4,7 @@ import com.example.habits.data.localdatasource.habits.DaysOfWeek
 import com.example.habits.data.localdatasource.habits.HabitEntity
 import com.example.habits.data.localdatasource.habits.HabitPriorityLevel
 import com.example.habits.data.localdatasource.habits.TimeOfTheDay
+import java.time.LocalTime
 import kotlin.random.Random
 
 fun generateMockHabit(): HabitEntity {
@@ -12,14 +13,26 @@ fun generateMockHabit(): HabitEntity {
     val repetitionsPerDay = Random.nextInt(from = 1, until = 10)
     val completedRepetitions = Random.nextInt(from = 0, until = repetitionsPerDay)
     val habitPriorityLevel = HabitPriorityLevel.values().random()
+    val reminderTimes =
+        listOf<LocalTime>(
+            LocalTime.now().plusSeconds(5),
+            LocalTime.now().plusSeconds(10),
+        )
 
     return HabitEntity(
         name = habitName,
         timeOfTheDay = timeOfTheDay,
         repetitionsPerDay = repetitionsPerDay,
-        daysToRepeat = listOf(DaysOfWeek.FRIDAY, DaysOfWeek.MONDAY),
+        daysToRepeat =
+            listOf(
+                DaysOfWeek.WEDNESDAY,
+                DaysOfWeek.FRIDAY,
+                DaysOfWeek.MONDAY,
+                DaysOfWeek.SATURDAY,
+            ),
         completedRepetitions = completedRepetitions,
         priorityLevel = habitPriorityLevel,
+        reminderTimes = reminderTimes,
     )
 }
 
