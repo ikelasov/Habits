6186fef04a0a27bfffb8525e04587e6aa4ff71dd
ilias.kelasidis <ikelasid@gmail.com>
2024-03-13 12:33:00 +0100

    Implement Configuration.Provider for WorkManager

--- app/src/main/java/com/example/HabitsApplication.kt
+++ app/src/main/java/com/example/HabitsApplication.kt
@@ -1,7 +1,21 @@
 package com.example
 
 import android.app.Application
+import android.util.Log
+import androidx.work.Configuration
+import com.example.habits.worker.HabitRemindersWorkerFactory
 import dagger.hilt.android.HiltAndroidApp
+import javax.inject.Inject
 
 @HiltAndroidApp
-class HabitsApplication : Application()
+class HabitsApplication : Application(), Configuration.Provider {
+    @Inject
+    lateinit var customWorkerFactory: HabitRemindersWorkerFactory
+
+    override val workManagerConfiguration: Configuration
+        get() =
+            Configuration.Builder()
+                .setMinimumLoggingLevel(Log.DEBUG)
+                .setWorkerFactory(customWorkerFactory)
+                .build()
+}
