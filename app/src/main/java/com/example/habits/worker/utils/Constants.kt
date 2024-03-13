2c04353c5538a1a0a2d98b862fdf98954513728e
ilias.kelasidis <ikelasid@gmail.com>
2024-03-13 12:20:43 +0100

    Create Constants for worker & notifications

---
+++ app/src/main/java/com/example/habits/worker/utils/Constants.kt
@@ -0,0 +1,15 @@
+package com.example.habits.worker.utils
+
+// region WorkManager
+
+const val WORK_MANAGER_REMINDER_TAG = "WORK_MANAGER_REMINDER_TAG"
+const val INPUT_DATA_HABIT_ID = "INPUT_DATA_HABIT_ID"
+
+// endregion
+
+// region Notifications
+
+const val HABIT_REMINDER_CHANNEL_ID = "HABIT_REMINDER_CHANNEL_ID"
+const val HABIT_REMINDER_CHANNEL_NAME = "Habits reminder notifications"
+
+// endregion
