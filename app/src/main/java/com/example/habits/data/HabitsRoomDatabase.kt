4b79a8421560618869f3b43444b95974fb7073c0
ikelasov <104062542+ikelasov@users.noreply.github.com>
2025-07-20 23:30:33 +0200

    make db populate with initial categories

--- app/src/main/java/com/example/habits/data/HabitsRoomDatabase.kt
+++ app/src/main/java/com/example/habits/data/HabitsRoomDatabase.kt
@@ -5,14 +5,22 @@ import androidx.room.Database
 import androidx.room.Room
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
+import androidx.sqlite.db.SupportSQLiteDatabase
 import com.example.habits.data.localdatasource.habits.HabitDao
 import com.example.habits.data.localdatasource.habits.HabitEntity
 import com.example.habits.data.localdatasource.habitscategory.HabitCategoryDao
 import com.example.habits.data.localdatasource.habitscategory.HabitCategoryEntity
 import com.example.habits.data.localdatasource.typeconverters.DaysOfWeekTypeConverter
 import com.example.habits.data.localdatasource.typeconverters.ReminderTimeTypeConverter
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-@Database(entities = [HabitEntity::class, HabitCategoryEntity::class], version = 1, exportSchema = false)
+@Database(
+    entities = [HabitEntity::class, HabitCategoryEntity::class],
+    version = 1,
+    exportSchema = false
+)
 @TypeConverters(DaysOfWeekTypeConverter::class, ReminderTimeTypeConverter::class)
 abstract class HabitsRoomDatabase : RoomDatabase() {
     abstract fun habitDao(): HabitDao
@@ -31,9 +39,37 @@ abstract class HabitsRoomDatabase : RoomDatabase() {
                         "habits_database",
                     )
                     .fallbackToDestructiveMigration()
+                    .addCallback(InitialCategoriesCallback())
                     .build()
                 INSTANCE = instance
                 return instance
+            }
+        }
+    }
+
+    private class InitialCategoriesCallback : Callback() {
+        override fun onCreate(db: SupportSQLiteDatabase) {
+            super.onCreate(db)
+            INSTANCE?.let { database ->
+                CoroutineScope(Dispatchers.IO).launch {
+                    populateCategories(database.habitCategoryDao())
+                }
+            }
+        }
+
+        suspend fun populateCategories(habitCategoryDao: HabitCategoryDao) {
+            // 3. Define the list of default categories.
+            val defaultCategories = listOf(
+                HabitCategoryEntity(name = "Fitness"),
+                HabitCategoryEntity(name = "Health"),
+                HabitCategoryEntity(name = "Work"),
+                HabitCategoryEntity(name = "Hobbies"),
+                HabitCategoryEntity(name = "Personal Growth")
+            )
+
+            // 4. Insert them into the database.
+            defaultCategories.forEach { category ->
+                habitCategoryDao.insertCategory(category)
             }
         }
     }
