65b00b1692e4101774ee2c62af482f8e98eca833
ikelasov <104062542+ikelasov@users.noreply.github.com>
2025-08-09 11:35:03 +0200

    chore: connect to firebase project

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,6 +4,7 @@ plugins {
     id("org.jetbrains.kotlin.android") version "1.8.10" apply false
     id("com.google.devtools.ksp") version "1.9.0-1.0.12" apply false
     id("com.google.dagger.hilt.android") version "2.44" apply false
+    id("com.google.gms.google-services") version "4.4.3" apply false
 }
 
 buildscript {
