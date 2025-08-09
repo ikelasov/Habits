941f87263d3f1b1b719c9a484fdca16d72b63e10
ikelasov <104062542+ikelasov@users.noreply.github.com>
2025-08-09 16:15:27 +0200

    feat: Update HabitCategoryUseCase

--- app/src/main/java/com/example/habits/domain/HabitCategoryUseCase.kt
+++ app/src/main/java/com/example/habits/domain/HabitCategoryUseCase.kt
@@ -3,22 +3,32 @@ package com.example.habits.domain
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
 import com.example.habits.data.localdatasource.habitscategory.HabitCategoryEntity
-import com.example.habits.data.repository.HabitsRepository
+import com.example.habits.data.repository.CategoryRepository
+import com.google.firebase.auth.FirebaseAuth
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
 
 class HabitCategoryUseCase @Inject constructor(
-    private val habitsRepository: HabitsRepository
+    private val categoryRepository: CategoryRepository,
+    firebaseAuth: FirebaseAuth
 ) {
 
+    private val userId = firebaseAuth.currentUser?.uid
+
     fun getCategoriesFlow(): Flow<List<HabitCategoryEntity>> {
-        return habitsRepository.getCategories()
+        return categoryRepository.getCategoriesForUserFlow(userId ?: "")
     }
 
-    suspend fun createCategory(categoryName: String, color: Color) {
-        if (categoryName.isBlank()) return
+    suspend fun createCategory(categoryName: String, color: Color): Result<Unit> {
+        val currentUserId = userId ?: return Result.failure(IllegalStateException("User not logged in"))
+        if (categoryName.isBlank()) return Result.failure(IllegalArgumentException("Category name cannot be blank"))
 
-        val newCategory = HabitCategoryEntity(name = categoryName, color = color.toArgb())
-        habitsRepository.insertCategory(newCategory)
+        val hexColor = String.format("#%06X", (0xFFFFFF and color.toArgb()))
+
+        return categoryRepository.createCategory(
+            userId = currentUserId,
+            categoryName = categoryName,
+            colorHex = hexColor
+        )
     }
 }
