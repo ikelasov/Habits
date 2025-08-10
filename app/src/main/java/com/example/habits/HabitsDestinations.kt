07fdfe2fae9da13f34d213a74460fbdfba32b7a4
ikelasov <104062542+ikelasov@users.noreply.github.com>
2025-08-10 12:29:37 +0200

    feat: add Manage Habits/Categories Nav Destinations

--- app/src/main/java/com/example/habits/HabitsDestinations.kt
+++ app/src/main/java/com/example/habits/HabitsDestinations.kt
@@ -5,4 +5,6 @@ sealed class HabitsDestinations(val route: String) {
     data object CreateHabitScreen : HabitsDestinations(route = "add_habit")
     data object LoginScreen : HabitsDestinations(route = "login")
     data object SignUpScreen : HabitsDestinations(route = "signup")
+    data object ManageHabitsScreen : HabitsDestinations(route = "manage_habits")
+    data object ManageCategoriesScreen : HabitsDestinations(route = "manage_categories")
 }
