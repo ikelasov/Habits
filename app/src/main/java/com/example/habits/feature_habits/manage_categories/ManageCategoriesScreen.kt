a339bacbda26ea60e8e43e2fcf8c2f04dd477081
ikelasov <104062542+ikelasov@users.noreply.github.com>
2025-08-10 12:28:37 +0200

    feat: Create empty Manage Habits/Categories screens

---
+++ app/src/main/java/com/example/habits/feature_habits/manage_categories/ManageCategoriesScreen.kt
@@ -0,0 +1,9 @@
+package com.example.habits.feature_habits.manage_categories
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ManageCategoriesScreen() {
+    Text(text = "Manage Categories Screen")
+}
---
+++ app/src/main/java/com/example/habits/feature_habits/manage_habits/ManageHabitsScreen.kt
@@ -0,0 +1,9 @@
+package com.example.habits.feature_habits.manage_habits
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+
+@Composable
+fun ManageHabitsScreen() {
+    Text(text = "Manage Habits Screen")
+}
