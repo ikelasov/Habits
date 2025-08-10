1e21d632066f13c4c9574a31516439a577cf57b9
ikelasov <104062542+ikelasov@users.noreply.github.com>
2025-08-10 15:11:04 +0200

    feat: add safe content padding on manage habits screen's content

--- app/src/main/java/com/example/habits/feature_habits/manage_habits/ManageHabitsScreen.kt
+++ app/src/main/java/com/example/habits/feature_habits/manage_habits/ManageHabitsScreen.kt
@@ -15,6 +15,7 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.paddingFromBaseline
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.lazy.LazyColumn
@@ -74,7 +75,7 @@ fun ManageHabitsScreen(
                     IconButton(onClick = onMenuClicked) {
                         Icon(Icons.Filled.Menu, contentDescription = "Open navigation drawer")
                     }
-                }
+                },
             )
         }
     ) { innerPadding ->
@@ -82,6 +83,7 @@ fun ManageHabitsScreen(
             modifier = Modifier
                 .fillMaxSize()
                 .padding(innerPadding)
+                .safeContentPadding()
         ) {
             if (isLoading) {
                 CircularProgressIndicator(modifier = Modifier.align(Alignment.Center))
